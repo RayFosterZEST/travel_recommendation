d014934cc53dc9943f90f0a716949dc46e3b4dda
ray foster <ray.foster@Zestplan.com>
2026-05-01 08:49:10 -0400

    js worked on

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -13,21 +13,50 @@ const fetchJSONFile = function(){
     })
 }
 
+async function renderSearch(items){
+    const searchResults = document.getElementById('searchResults');
+    if(items.length>0){
+        let searchHTML = "";
+        for(let item of items){
+            searchHTML+=`
+            <div class="searchResultItem">
+                <img src="${item?.imageUrl}" class="searchURL" />
+                <h3>${item?.name || 'Error'}</h3>
+                <p>${item?.description}</p>
+            </div>
+            `
+        }
+        searchResults.innerHTML=searchHTML;
+    }else{
+        searchResults.innerHTML=`<span>No Results Found</span>`
+    }
+console.log(items)
+}
+
 async function searchLocation(){
     const results = await fetchJSONFile();
     if(!results.beaches || !results.countries || !results.temples){
         alert('Something went wrong');
         return;
     };
+    console.log(results);
     const searchQuery = document.getElementById('searchInput')?.value ||'';
     const keySearch = searchQuery.toLowerCase();
 
-    let foundKey = undefined;
-    Object.keys(results).forEach(function(key){
-        if(keySearch.indexOf(key)!==-1)foundKey=key;
-    })
+    let searchResults = [];
+
+    switch(keySearch){
+        case 'beach': searchResults = results.beaches;break;
+        case 'beaches':searchResults = results.beaches;break;
+        case 'sand':searchResults = results.beaches;break;
+        case 'temples':searchResults=results.temples;break;
+        case 'temple':searchResults=results.temples;break;
+        case 'church':searchResults = results.temples;break;
+    }
     
-    
-    
-    console.log(searchQuery)
+    for(let country of results.countries){
+        if(country.name.toLowerCase()===keySearch)searchResults = country.cities;
+    }
+
+    renderSearch(searchResults)
 }
